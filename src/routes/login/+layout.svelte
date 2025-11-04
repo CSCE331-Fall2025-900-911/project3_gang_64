f181862700761bae2b53321129a6ae4bc06eca51
bwees <brandonwees@gmail.com>
2025-11-04 13:56:57 -0600

    chore: fix broken import

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,46 +1,16 @@
 <script lang="ts">
-  import { page } from '$app/state';
   import favicon from '$lib/assets/favicon.svg';
-  import logo from '$lib/assets/logo.png';
-  import UserModal from '$lib/shared/UserModal.svelte';
-  import {
-    AppShell,
-    AppShellHeader,
-    Avatar,
-    initializeTheme,
-    modalManager,
-    Select,
-    ThemeSwitcher,
-    type SelectItem,
-  } from '@immich/ui';
+  import { initializeTheme } from '@immich/ui';
   import '../../app.css';
 
   let { children } = $props();
 
   initializeTheme();
-
-  const modes: SelectItem[] = [
-    { label: 'Manager', value: 'manager' },
-    { label: 'Cashier', value: 'cashier' },
-  ];
-
-  let mode = $derived(modes.find((item) => item.value === page.url.pathname.split('/')[2]) ?? modes[0]);
-
-  function handleModeChange(selected: SelectItem) {
-    window.location.href = `/kitchen/${selected.value}/`;
-  }
-
-  function openUserProfile() {
-    modalManager.show(UserModal);
-  }
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <title>ShareTea | {mode.label}</title>
+  <title>ShareTea | Login</title>
 </svelte:head>
 
-<AppShell>
-
-  {@render children?.()}
-</AppShell>
+{@render children?.()}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
   import { page } from '$app/state';
-  import { Card, CardTitle, CardHeader , Button, CardBody} from '@immich/ui';
-  import google from '$lib/assets/google.png' ;
-  import logo from  '$lib/assets/logo.png'
+  import google from '$lib/assets/google.png';
+  import logo from '$lib/assets/logo.png';
+  import { Button, Card, CardBody, CardHeader, CardTitle } from '@immich/ui';
   console.log(page.url.hash.replace('#', ''));
 </script>
 
 <div class="flex justify-center">
-  <div class="bg-black align-center mx-5 my-3 size-fit">
-    <img class = "w-100 my-10" src={logo} alt= "Share Tea"/>
-  <Card >
-    <CardHeader>
-      <CardTitle>Login to Sharetea:</CardTitle>
-    </CardHeader>
-    <CardBody>
-        <Button class = "h-25 justify-self-center">
-          <img class = "h-20" src = {google} alt= "google logo" />
+  <div class="align-center mx-5 my-3 size-fit bg-black">
+    <img class="my-10 w-100" src={logo} alt="Share Tea" />
+    <Card>
+      <CardHeader>
+        <CardTitle>Login to Sharetea:</CardTitle>
+      </CardHeader>
+      <CardBody>
+        <Button class="h-25 justify-self-center">
+          <img class="h-20" src={google} alt="google logo" />
           Continue with Google
         </Button>
-    </CardBody>
-  </Card>
+      </CardBody>
+    </Card>
   </div>
 </div>
