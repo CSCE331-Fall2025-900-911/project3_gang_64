8dbd370bbce3718649afe6cef26a9d355484be35
Matthew-C-Warren <mcwarren922@tamu.edu>
2025-11-04 13:58:29 -0600

    feat: update auth.ts with env

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,9 +1,10 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import { getDB } from '../db';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   adapter: DrizzleAdapter(getDB()),
-  providers: [Google],
+  providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
 });
