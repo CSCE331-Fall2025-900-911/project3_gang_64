639e1a12f54455fef1dad7d887f7d00a4ef8ce49
bwees <brandonwees@gmail.com>
2025-11-05 12:47:36 -0600

    fix: theming for login/unauthorized

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex justify-center">
-  <div class="align-center mx-5 my-3 size-fit bg-black">
+  <div class="align-center mx-5 my-3 size-fit">
     <img class="mx-auto my-10 w-100" src={logo} alt="Share Tea" />
     <Card>
       <CardHeader>
