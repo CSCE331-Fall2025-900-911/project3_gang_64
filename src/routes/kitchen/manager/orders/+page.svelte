1ded0b49ede62db1fa0668ee8d387e70f450be7e
RanivG-Tamu <rgupt@tamu.edu>
2025-11-05 19:07:49 -0600

    feat: get order table rows

---
+++ src/routes/kitchen/manager/orders/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from "flowbite-svelte"; 
+  import {getOrders} from "$lib/api/orders.remote"
+  import { type OrderRow} from "$lib/db/types";
+  import { number } from "valibot";
+  import { Button } from "@immich/ui";
+
+  async function loadOrders() {
+    try {
+      let orders = await getOrders();
+      console.log(orders);
+    }
+    catch (err) {
+      console.error("fuck", err);
+    }
+  }
+
+  loadOrders()
+</script>
+
+<Table>
+  <TableHead>
+    <TableHeadCell>Order #</TableHeadCell>
+    <TableHeadCell>Amount</TableHeadCell>
+    <TableHeadCell>Time</TableHeadCell>
+    <TableHeadCell>Name</TableHeadCell>
+    <TableHeadCell>Customer Contact</TableHeadCell>
+    <TableHeadCell>Payment Method</TableHeadCell>
+  </TableHead>
+</Table>
+
+<Button on:click={loadOrders}>Click to load order rows to console</Button>
