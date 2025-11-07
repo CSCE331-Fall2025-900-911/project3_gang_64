40bfb26c884d7ed15d6c983ac780c7bb842a9508
Matthew-C-Warren <mcwarren922@tamu.edu>
2025-11-06 20:53:00 -0600

    fix: refactor kiosk layout

--- src/routes/kiosk/cart/+page.svelte
+++ src/routes/kiosk/cart/+page.svelte
@@ -1,70 +1,14 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
-  import favicon from '$lib/assets/favicon.svg';
-  import logo from '$lib/assets/logo.png';
+  import { AppShell, Button, Heading, HStack, modalManager, Text } from '@immich/ui';
   import { kioskManager } from '$lib/managers/kiosk.svelte';
-  import {
-    AppShell,
-    AppShellHeader,
-    Avatar,
-    Button,
-    Heading,
-    HStack,
-    IconButton,
-    initializeTheme,
-    modalManager,
-    Text,
-    ThemeSwitcher,
-  } from '@immich/ui';
-  import { mdiShoppingOutline } from '@mdi/js';
-  import '../../../app.css';
   import OrderSubmitDialog from './KioskOrderSubmit.svelte';
-
-  let { children } = $props();
-
-  initializeTheme();
-
-  function closeCart() {
-    goto('/kiosk/order');
-  }
 
   function showSubmitDialog() {
     modalManager.show(OrderSubmitDialog);
   }
 </script>
 
-<svelte:head>
-  <link rel="icon" href={favicon} />
-  <title>ShareTea</title>
-</svelte:head>
-
 <AppShell>
-  <AppShellHeader>
-    <div class="flex w-full items-center justify-between p-4">
-      <img src={logo} alt="ShareTea Logo" class="h-6" />
-      <div class="flex items-center gap-4">
-        <ThemeSwitcher />
-        <div class="relative inline-block">
-          <IconButton
-            icon={mdiShoppingOutline}
-            shape="round"
-            size="medium"
-            color="primary"
-            onclick={closeCart}
-            aria-label="Cart"
-          />
-          <div class="absolute -top-1 -right-1">
-            <Avatar
-              size="tiny"
-              name={kioskManager.getCurrentCartAmount() > 9 ? '9 +' : kioskManager.getCurrentCartAmount().toString()}
-              color="red"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  </AppShellHeader>
-
   <div class="flex flex-col overflow-hidden" style="height: 80vh;">
     <div class="bg-level-1 mt-4 mr-80 ml-80 flex flex-1 flex-col overflow-y-auto rounded-xl p-3">
       {#if kioskManager.currentOrder.length == 0}
@@ -107,7 +51,5 @@
         >Submit Order</Button
       >
     </div>
-
-    {@render children?.()}
   </div>
 </AppShell>
