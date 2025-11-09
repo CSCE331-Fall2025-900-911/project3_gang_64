a792cd2c2b01f8fa9c01cf8d845666b53b47d702
bwees <brandonwees@gmail.com>
2025-11-08 18:15:17 -0600

    chore: reports page structure

--- src/routes/kitchen/manager/reports/+page.svelte
+++ src/routes/kitchen/manager/reports/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Reports</h1>
+<script>
+  import { Card, CardBody, CardHeader, CardTitle, Heading, Text } from '@immich/ui';
+</script>
+
+<Heading size="large" class="mt-2 mb-6">Reports</Heading>
+
+<div class="mt-4 flex flex-col gap-4">
+  <Card expandable>
+    <CardHeader>
+      <CardTitle>X-Report</CardTitle>
+    </CardHeader>
+    <CardBody>
+      <Text>X-Report information</Text>
+    </CardBody>
+  </Card>
+
+  <Card expandable>
+    <CardHeader>
+      <CardTitle>Z-Report</CardTitle>
+    </CardHeader>
+    <CardBody>
+      <Text>Z-Report information</Text>
+    </CardBody>
+  </Card>
+</div>
