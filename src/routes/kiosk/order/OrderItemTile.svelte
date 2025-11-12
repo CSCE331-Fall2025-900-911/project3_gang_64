3c7c1ebc4a8b1e892c83eda112cbd3b523c2d84f
RanivG-Tamu <rgupt@tamu.edu>
2025-11-12 14:38:20 -0600

    Feat: disabled orders for out of stock

--- src/routes/kiosk/order/OrderItemTile.svelte
+++ src/routes/kiosk/order/OrderItemTile.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import type { MenuItem } from '$lib/db/types';
   import { kioskManager } from '$lib/managers/kiosk.svelte';
-  import { Heading, IconButton } from '@immich/ui';
+  import { Heading, IconButton , LoadingSpinner } from '@immich/ui';
   import { mdiPlus } from '@mdi/js';
+  import {getIngredients , getIngredientsForMenuItem} from '$lib/api/ingredient.remote';
+  import {onMount} from 'svelte';
+  import { boolean } from 'drizzle-orm/gel-core';
 
   interface Props {
     item: MenuItem;
@@ -12,6 +15,28 @@
   let { item, firstAddAction }: Props = $props();
   let loading = $state(false);
 
+  let inventory = getIngredients();
+  let recipe = $derived(getIngredientsForMenuItem(item.id));
+  let outOfStock = $state(false);
+  let isOut:boolean;
+  
+  //cycle through ingredients in recipe see if we have inventory, set stock appropriately
+  $effect(() => {
+    let isOut = false;
+    recipe.current?.forEach(function(ingredient) {
+      inventory.current?.forEach(function (stockItem) {
+        if (ingredient.id == stockItem.id && stockItem.currentStock == 0) {
+          isOut = true;
+        }
+      });
+    });
+
+    if (isOut !== outOfStock) outOfStock = isOut;
+
+  });
+
+
+
   async function handleAddToOrder() {
     loading = true;
     await kioskManager.addToOrder(item);
@@ -20,7 +45,12 @@
       firstAddAction();
     }
   }
+
+  console.log("out of stock" , outOfStock);
+  console.log("loading" , loading);
+  
 </script>
+
 
 <div class="flex flex-col justify-between rounded-lg border-2 p-4">
   <img
@@ -36,7 +66,7 @@
       size="small"
       color="info"
       onclick={handleAddToOrder}
-      disabled={loading}
+      disabled={outOfStock || loading}
       aria-label="Add to Order"
     />
   </div>
