f598758f6553825c91345b16c4340e3d702d2f40
RanivG-Tamu <rgupt@tamu.edu>
2025-11-14 10:35:17 -0600

    feat: disabled orders for out of stock (#68)
    Co-authored-by: R G <ranivgupta@gmail.com>

--- src/routes/kiosk/order/OrderItemTile.svelte
+++ src/routes/kiosk/order/OrderItemTile.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import type { MenuItem } from '$lib/db/types';
   import { kioskManager } from '$lib/managers/kiosk.svelte';
-  import { Heading, IconButton } from '@immich/ui';
+  import { Heading, IconButton} from '@immich/ui';
   import { mdiPlus } from '@mdi/js';
+  import {getIngredients , getIngredientsForMenuItem} from '$lib/api/ingredient.remote';
 
   interface Props {
     item: MenuItem;
@@ -12,6 +13,21 @@
   let { item, firstAddAction }: Props = $props();
   let loading = $state(false);
 
+  let inventory = getIngredients();
+  let recipe = $derived(getIngredientsForMenuItem(item.id));
+  let outOfStock = $derived.by(() => {
+    let tempStock = false;
+    recipe.current?.forEach(ingredient => {
+      inventory.current?.forEach(stockItem => {
+        if(stockItem.id === ingredient.id && stockItem.currentStock == 0) {
+          tempStock = true;
+          return;
+        }
+      });
+    })
+    return tempStock;
+  });
+
   async function handleAddToOrder() {
     loading = true;
     await kioskManager.addToOrder(item);
@@ -20,14 +36,18 @@
       firstAddAction();
     }
   }
+  
 </script>
 
-<div class="flex flex-col justify-between rounded-lg border-2 p-4">
+<div class="flex flex-col justify-between rounded-lg border-2 p-4 relative">
   <img
     src={item.imageUrl ?? '/kioskImages/boba.jpeg'}
     alt={item.name}
     class="mb-3 h-50 w-full rounded-md border object-cover"
   />
+  {#if (outOfStock)}
+    <div class="bg-black/50 text-white absolute top-25 text-center w-full pr-9">Out of Stock</div>
+  {/if}
   <Heading size="medium" class="mb-2">{item.name}</Heading>
   <div class="mt-4 flex items-center justify-between">
     <Heading size="small" fontWeight="normal">${item.price.toFixed(2)}</Heading>
@@ -36,7 +56,7 @@
       size="small"
       color="info"
       onclick={handleAddToOrder}
-      disabled={loading}
+      disabled={outOfStock || loading}
       aria-label="Add to Order"
     />
   </div>
--- src/routes/kitchen/cashier/MenuItemTile.svelte
+++ src/routes/kitchen/cashier/MenuItemTile.svelte
@@ -2,6 +2,8 @@
   import type { MenuItem } from '$lib/db/types';
   import { cashierManager } from '$lib/managers/cashier.svelte';
   import { Button, Heading } from '@immich/ui';
+  import {getIngredients , getIngredientsForMenuItem} from '$lib/api/ingredient.remote';
+
 
   interface Props {
     item: MenuItem;
@@ -10,18 +12,41 @@
   let { item }: Props = $props();
   let loading = $state(false);
 
+  let inventory = getIngredients();
+  let recipe = $derived(getIngredientsForMenuItem(item.id));
+  let outOfStock = $derived.by(() => {
+    let tempStock = false;
+    recipe.current?.forEach(ingredient => {
+      inventory.current?.forEach(stockItem => {
+        if(stockItem.id === ingredient.id && stockItem.currentStock == 0) {
+          tempStock = true;
+          return;
+        }
+      });
+    })
+    return tempStock;
+    
+  });
+
   async function handleAddToOrder(item: MenuItem) {
     loading = true;
 
     await cashierManager.addToOrder(item);
     loading = false;
   }
+
 </script>
 
 <div class="flex flex-col justify-between rounded-lg border p-4">
   <Heading size="medium" class="mb-2">{item.name}</Heading>
   <div class="mt-4 flex items-center justify-between">
     <Heading size="small" fontWeight="normal">${item.price.toFixed(2)}</Heading>
-    <Button onclick={() => handleAddToOrder(item)} {loading}>Add to Order</Button>
+    <Button onclick={() => handleAddToOrder(item)} {loading} disabled = {outOfStock}>
+      {#if (outOfStock)}
+        Out of Stock 
+      {:else}
+        Add to Order  
+      {/if}
+    </Button>
   </div>
 </div>
