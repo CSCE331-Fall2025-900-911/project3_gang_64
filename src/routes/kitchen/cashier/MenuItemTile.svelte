716bc1fdc128f0964c4963b57dd81606fe170e18
RanivG-Tamu <rgupt@tamu.edu>
2025-11-17 14:43:15 -0600

    chore: stack price and add to order button to reduce width

--- src/routes/kitchen/cashier/MenuItemTile.svelte
+++ src/routes/kitchen/cashier/MenuItemTile.svelte
@@ -33,10 +33,10 @@
   }
 </script>
 
-<div class="flex flex-col justify-between rounded-lg border p-4 w-fit">
+<div class="flex flex-col justify-between rounded-lg border p-4">
   <Heading size="medium" class="mb-2">{item.name}</Heading>
-  <div class="mt-4 flex items-center justify-between">
-    <Heading size="medium" fontWeight="normal" class="mr-2">${item.price.toFixed(2)}</Heading>
+  <div class="mt-4 flex justify-between flex-col">
+    <Heading size="medium" fontWeight="normal" class="mb-2">${item.price.toFixed(2)}</Heading>
     <Button onclick={handleAddToOrder} {loading} disabled={outOfStock} size="large">
       {#if outOfStock}
         Out of Stock
