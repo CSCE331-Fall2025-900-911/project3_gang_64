403721e7c5e7ee2df77419bf0db416fd75215226
tobiasbui <tobiasbui@tamu.edu>
2025-11-20 11:25:25 -0600

    feat: added item totals

--- src/routes/kiosk/order/ItemModification.svelte
+++ src/routes/kiosk/order/ItemModification.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { orderManager } from '$lib/managers/order_manager.svelte';
+  import { getIngredientsForMenuItem } from '$lib/api/ingredient.remote';
   import type { ModalProps } from '$lib/utils/utils';
-  import { Button, Modal, ModalBody, ModalFooter } from '@immich/ui';
-  import { mdiInvoiceSend } from '@mdi/js';
+  import { Button, Modal, ModalBody, ModalFooter, HStack, Heading, IconButton, Text } from '@immich/ui';
+  import NumberStepper from '$lib/components/NumberStepper.svelte';
+  import { mdiInvoiceSend, mdiPencil, mdiTrashCan } from '@mdi/js';
   import type { MenuItem } from '$lib/db/types';
   import { t } from '$lib/utils/utils';
 
@@ -13,6 +15,8 @@
 
   let { onClose, item, states }: ModalProps & Props = $props();
   let loading = $state(false);
+  let currentPrice = $state(item.price);
+  let ingredientList = $state(getIngredientsForMenuItem(item.id).current);
 
   async function handleAddToOrder() {
     loading = true;
@@ -33,7 +37,27 @@
 
 <Modal title={t('kiosk_itemModification')} icon={mdiInvoiceSend} {onClose}>
   <ModalBody>
-    <!-- Fill this in -->
+    <div class="m-6 flex w-1/3 flex-col">
+      <div class="bg-level-1 mb-4 flex flex-1 flex-col overflow-y-auto rounded-xl p-3">
+        <div>
+          <Heading size="small">{item.name}</Heading>
+          <div class="gap-3 pl-4">
+            {#each ingredientList as ingredient}
+              <Text size="small" class="text-muted-foreground">
+                {ingredient.name}
+              </Text>
+            {/each}
+          </div>
+        </div>
+      </div>
+
+      <div class="shrink-0 gap-2">
+        <HStack class="flex justify-between">
+          <Heading size="tiny" fontWeight="normal">{t('kiosk_itemTotal')}</Heading>
+          <p>${currentPrice.toFixed(2)}</p>
+        </HStack>
+      </div>
+    </div>
   </ModalBody>
   <ModalFooter>
     <div class="grid w-full grid-cols-1 gap-2">
