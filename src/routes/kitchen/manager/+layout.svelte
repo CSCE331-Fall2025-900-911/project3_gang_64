2f521745100182d0eda10050a0a9bb659559eb82
bwees <brandonwees@gmail.com>
2025-11-20 00:49:52 -0600

    feat: manager tab groups

--- src/routes/kitchen/manager/+layout.svelte
+++ src/routes/kitchen/manager/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  import { AppShellSidebar, NavbarItem } from '@immich/ui';
   import { t } from '$lib/utils/utils';
+  import { AppShellSidebar, NavbarGroup, NavbarItem } from '@immich/ui';
   import {
     mdiAccount,
     mdiBadgeAccount,
+    mdiCurrencyUsd,
     mdiFileDocument,
     mdiGauge,
     mdiPackageVariantClosed,
@@ -16,12 +17,13 @@
 <div>
   <AppShellSidebar class="gap-2 pt-2 pr-4">
     <NavbarItem href="/kitchen/manager/dashboard" title={t('manager_dashboard')} icon={mdiGauge} />
-    <!-- TODO: Implement Reports -->
-    <!-- <NavbarItem href="/kitchen/manager/reports" title="Reports" icon={mdiCurrencyUsd} /> -->
     <NavbarItem href="/kitchen/manager/employees" title={t('manager_employees')} icon={mdiBadgeAccount} />
-    <NavbarItem href="/kitchen/manager/menu_items" title={t('manager_menu_items')} icon={mdiSilverware} />
+    <NavbarGroup title="Sales" />
     <NavbarItem href="/kitchen/manager/orders" title={t('manager_orders')} icon={mdiFileDocument} />
     <NavbarItem href="/kitchen/manager/customers" title={t('manager_customers')} icon={mdiAccount} />
+    <NavbarItem href="/kitchen/manager/reports" title="Reports" icon={mdiCurrencyUsd} />
+    <NavbarGroup title="Menu" />
+    <NavbarItem href="/kitchen/manager/menu_items" title={t('manager_menu_items')} icon={mdiSilverware} />
     <NavbarItem href="/kitchen/manager/inventory" title={t('manager_inventory')} icon={mdiPackageVariantClosed} />
   </AppShellSidebar>
 
