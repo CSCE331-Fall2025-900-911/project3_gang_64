e7738f2173c85170b062189b78396f29c24b9d3f
bwees <brandonwees@gmail.com>
2025-12-05 13:26:37 -0600

    chore: remove theme selector

--- src/routes/kiosk/+layout.svelte
+++ src/routes/kiosk/+layout.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
   import favicon from '$lib/assets/favicon.svg';
   import logo from '$lib/assets/logo.png';
+  import AccessibiltyModal from '$lib/components/AccessibiltyModal.svelte';
   import ColorblindFilter from '$lib/components/ColorblindFilter.svelte';
+  import LanguageSelectModal from '$lib/components/LanguageSelectModal.svelte';
   import { orderManager } from '$lib/managers/order_manager.svelte';
   import { t } from '$lib/utils/utils';
-  import {
-    AppShell,
-    AppShellHeader,
-    Avatar,
-    IconButton,
-    initializeTheme,
-    modalManager,
-    ThemeSwitcher,
-  } from '@immich/ui';
+  import { AppShell, AppShellHeader, Avatar, IconButton, initializeTheme, modalManager } from '@immich/ui';
   import { mdiCartOutline, mdiTranslate, mdiWheelchair } from '@mdi/js';
   import '../../app.css';
   import CartModal from './order/CartModal.svelte';
-  import LanguageSelectModal from '$lib/components/LanguageSelectModal.svelte';
-  import AccessibiltyModal from '$lib/components/AccessibiltyModal.svelte';
 
   let { children } = $props();
 
@@ -40,7 +32,6 @@
     <div class="flex w-full items-center justify-between p-4">
       <img src={logo} alt={t('logoAlt')} class="h-6" />
       <div class="flex items-center gap-4">
-        <ThemeSwitcher size="large" />
         <IconButton
           icon={mdiWheelchair}
           size="large"
