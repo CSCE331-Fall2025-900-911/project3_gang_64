d0290edb3b4fef9d0e9563b07fca6c141a23b6b5
bwees <brandonwees@gmail.com>
2025-12-04 21:15:30 -0600

    feat: portal

---
+++ src/routes/auth/directory/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import logo from '$lib/assets/logo.png';
+  import { Button, Card, CardBody, CardHeader, CardTitle, Stack } from '@immich/ui';
+  import { mdiSilverware, mdiTablet } from '@mdi/js';
+</script>
+
+<div class="flex justify-center">
+  <div class="align-center mx-5 my-3 size-fit">
+    <img class="mx-auto my-10 w-100" src={logo} alt="Share Tea" />
+    <Card>
+      <CardHeader>
+        <CardTitle>Interface Portal</CardTitle>
+      </CardHeader>
+      <CardBody>
+        <Stack>
+          <Button leadingIcon={mdiSilverware} href="/kitchen">Kitchen Interface</Button>
+          <Button leadingIcon={mdiTablet} href="/kiosk">Kiosk Interface</Button>
+        </Stack>
+      </CardBody>
+    </Card>
+  </div>
+</div>
