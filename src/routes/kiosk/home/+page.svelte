4af20bd2572dcec545a41a2ac41764cc03141433
RanivG-Tamu <rgupt@tamu.edu>
2025-12-05 18:49:34 -0600

    fix: home/timout scroll

--- src/routes/kiosk/home/+page.svelte
+++ src/routes/kiosk/home/+page.svelte
@@ -5,9 +5,9 @@
   const orderUrl: string = '/kiosk/order';
 </script>
 
-<div class="h-[100vh] w-[100vw] z-50 absolute overflow-visible" on:click={()=> goto(orderUrl)}>
+<div class="z-50 fix inset-0 overflow-hidden" on:click={()=> goto(orderUrl)}>
   <img src={home} alt="boba gallery" class="absolute object-cover size-full z-30"/>
-  <div class="w-full bg-black/50 text-5xl flex justify-center py-10 absolute top-175 z-35">
+  <div class="w-full bg-black/50 text-5xl flex justify-center py-10 absolute top-150 z-35">
     Click Anywhere to Begin
   </div>
 </div>
