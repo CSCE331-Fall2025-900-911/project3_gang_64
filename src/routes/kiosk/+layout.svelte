476d19684b2e56ddc382ec25b04001a8811aab09
RanivG-Tamu <rgupt@tamu.edu>
2025-12-07 13:59:23 -0600

    feat: kiosk timeout (#95)
    Co-authored-by: bwees <brandonwees@gmail.com>

--- src/routes/kiosk/+layout.svelte
+++ src/routes/kiosk/+layout.svelte
@@ -6,18 +6,100 @@
   import LanguageSelectModal from '$lib/components/LanguageSelectModal.svelte';
   import { orderManager } from '$lib/managers/order_manager.svelte';
   import { t } from '$lib/utils/utils';
-  import { AppShell, AppShellHeader, Avatar, IconButton, initializeTheme, modalManager } from '@immich/ui';
+  import { 
+    AppShell,
+    AppShellHeader,
+    Avatar,
+    Button,
+    IconButton,
+    initializeTheme,
+    Modal,
+    ModalBody,
+    ModalHeader,
+    modalManager,
+  } from '@immich/ui';
   import { mdiCartOutline, mdiTranslate, mdiWheelchair } from '@mdi/js';
+  import { onMount } from 'svelte';
   import '../../app.css';
   import CartModal from './order/CartModal.svelte';
+  import { onNavigate, goto } from '$app/navigation';
 
   let { children } = $props();
 
   initializeTheme();
 
+  const timeOutLength = 12;
+  let timer = $derived(timeOutLength);
+  let showModal = $derived(false);
+
   function openCart() {
     modalManager.show(CartModal);
   }
+
+  //timeout logic and set up
+  const resetTimer = () => {
+    timer = timeOutLength;
+    showModal = false;
+  };
+
+  const resetTimerEvent = () => {
+    if (!showModal) {
+      resetTimer();
+    }
+  };
+
+  const listenerSetup = () => {
+    const events = ['mousemove', 'keydown', 'click', 'touchstart'];
+    events.forEach((ev) => window.addEventListener(ev, resetTimerEvent));
+
+    return () => events.forEach((ev) => window.removeEventListener(ev, resetTimerEvent));
+  };
+
+  function countD() {
+    const listeners = listenerSetup();
+    timer = timeOutLength;
+
+    let countDown = setInterval(() => {
+      --timer;
+      if (!showModal && timer <= 10){
+        showModal = true;
+      }
+      if (timer <= 0) {
+        clearInterval(countDown);
+        listeners();
+        orderManager.clearOrder();
+        showModal = false;
+        window.location.href='/kiosk/home'
+
+      }
+    }, 1000);
+
+    return () => {
+      listeners();
+      clearInterval(countDown);
+    };
+  }
+
+  function timeOut() {
+    if (window.location.pathname !== '/kiosk/home') {
+      const runTimer = countD();
+
+      return () => {
+        runTimer();
+      };
+    } else return null;
+  }
+
+  onMount(() => {
+    //timeout
+    const time = timeOut();
+
+    return time;
+  });
+
+  onNavigate(() => timeOut);
+
+
 </script>
 
 <ColorblindFilter />
@@ -68,6 +150,19 @@
       </div>
     </div>
   </AppShellHeader>
+  {#if showModal}
+    <Modal class="w-90 text-center">
+      <ModalHeader>
+        <div class="my-3 text-4xl">Are you still there?</div>
+      </ModalHeader>
+      <ModalBody>
+        <div class="flex flex-col justify-center text-3xl">
+          Your order will be cleared in {timer} seconds
+          <Button onclick={resetTimer} class="my-5">I'm still here!</Button>
+        </div>
+      </ModalBody>
+    </Modal>
+  {/if}
 
   {@render children?.()}
 </AppShell>
