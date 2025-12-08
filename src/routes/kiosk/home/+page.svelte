a4c2e302e0a9456032450c33b68da5ced1866789
RanivG-Tamu <rgupt@tamu.edu>
2025-12-07 21:45:17 -0600

    chore: minor navigation and localization changes

--- src/routes/kiosk/home/+page.svelte
+++ src/routes/kiosk/home/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import home from '$lib/assets/home.webp';
+  import { t } from '$lib/utils/utils'
+  import {goto} from '$app/navigation'
 </script>
 
-<div class="fix inset-0 z-50 overflow-hidden" on:click={() => (window.location.href = '/kiosk/order')}>
+<div class="fix inset-0 z-50 overflow-hidden" on:click={() => goto('/kiosk/order')}>
   <img src={home} alt="boba gallery" class="absolute z-30 size-full object-cover" />
-  <div class="absolute top-150 z-35 flex w-full justify-center bg-black/50 py-10 text-5xl">Click Anywhere to Begin</div>
+  <div class="absolute top-150 z-35 flex w-full justify-center bg-black/50 py-10 text-5xl">{t('kiosk_timeOut')}</div>
 </div>
+asdfas
