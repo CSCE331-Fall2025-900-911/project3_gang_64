dad8532ff58ec006a706d0bde832285f3e1dbfa0
bwees <brandonwees@gmail.com>
2025-12-11 22:48:42 -0600

    fix: add keys to prevent dom element reuse

--- src/routes/kiosk/order/MenuGroup.svelte
+++ src/routes/kiosk/order/MenuGroup.svelte
@@ -15,7 +15,7 @@
   <h1 class="mb-4 text-3xl font-bold">{td(id)}</h1>
 
   <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-    {#each items as item}
+    {#each items as item (item.id)}
       <OrderItemTile {item} />
     {/each}
   </div>
